5a4b579465cdd6d7d28cfabd64075fb4870fdfef
Calvin Lee <calsmlee@uw.edu>
2016-09-20 11:11:06 -0700

    [feature] Switched from redux promise to thunk

--- client/routes.js
+++ client/routes.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
-import { Provider } from 'react-redux'
+import { applyMiddleware, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
-import App from './app.js'
-import SignUp from './components/signUp'
-import CreateJob from './components/createJob'
-import store from './reducers/mainReducer'
+import rootReducer from './reducers/mainReducer';
+import App from './app.js';
+import SignUp from './components/signUp';
+import CreateJob from './components/createJob';
 
 //Basic routing, to add another route just do:
 //<Route path='/insertUrl' component={insertComponentName} />
+const createStoreWithMiddleWare = applyMiddleware(thunk)(createStore);
+console.log('rootReducer', rootReducer);
+export const store = createStoreWithMiddleWare(rootReducer, window.devToolsExtension ? window.devToolsExtension() : f => f);
 
 render((
   <Provider store={store}>
